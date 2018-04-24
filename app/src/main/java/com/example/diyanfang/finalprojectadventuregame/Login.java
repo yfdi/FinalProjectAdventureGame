85535ed1bdbe86d6896d1874ed45eb1f7cf885c7
mclark414 <mclark@packer.edu>
2018-04-23 20:24:27 -0400

    In process of login page

--- app/src/main/java/com/example/diyanfang/finalprojectadventuregame/Login.java
+++ app/src/main/java/com/example/diyanfang/finalprojectadventuregame/Login.java
@@ -1,6 +1,18 @@
 package com.example.diyanfang.finalprojectadventuregame;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by mclark on 4/23/18.
@@ -8,4 +20,54 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
 
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private FirebaseAuth mAuth;
+    String email;
+    String password;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference userRef = database.getReference("user");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    }
+    public void logIn(View view){
+        email = (emailEditText.getText()).toString();
+        password = (passwordEditText.getText()).toString();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    Toast.makeText(Login.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(Login.this, task.getResult().getUser().getEmail() + "logged in successfully", Toast.LENGTH_SHORT).show();
+
+                    finish();
+                }
+            }
+        });
+    }
+    public void signUp(View view){
+        email = (emailEditText.getText()).toString();
+        password = (passwordEditText.getText()).toString();
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()){
+                    Toast.makeText(Login.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(Login.this, task.getResult().getUser().getEmail() + "sign up successfully", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        });
+        int level = 1;
+        User temp = new User(email, password, level);
+        userRef.push().setValue(temp);
+    }
 }
--- app/src/main/java/com/example/diyanfang/finalprojectadventuregame/User.java
+++ app/src/main/java/com/example/diyanfang/finalprojectadventuregame/User.java
@@ -17,4 +17,27 @@ public class User {
         this.level = level;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
 }
