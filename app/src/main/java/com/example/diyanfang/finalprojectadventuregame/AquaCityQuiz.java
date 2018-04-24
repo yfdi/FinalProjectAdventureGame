d8558e815d39ee79a7326297bb92029fda0201ec
yfdi <nicoledi.1122@gmail.com>
2018-04-24 11:46:21 -0400

    add aqua city quiz xml

---
+++ app/src/main/java/com/example/diyanfang/finalprojectadventuregame/AquaCityQuiz.java
@@ -0,0 +1,13 @@
+package com.example.diyanfang.finalprojectadventuregame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AquaCityQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aqua_city_quiz);
+    }
+}
